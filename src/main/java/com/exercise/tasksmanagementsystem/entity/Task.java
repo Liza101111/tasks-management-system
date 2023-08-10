eef50135c0ca4004a45b93b302af6c96ed783a03
Liza <lixia101111@gmail.com>
2023-08-10 14:49:34 +0300

    Created task entity

---
+++ src/main/java/com/exercise/tasksmanagementsystem/entity/Task.java
@@ -0,0 +1,8 @@
+package com.exercise.tasksmanagementsystem.entity;
+
+public class Task {
+    private String name;
+    private int timeSpent;
+    private String taskGroup;
+    private String assignee;
+}
