f649691747cee8638d0caedaaba5af44d1a8e98e
Liza <lixia101111@gmail.com>
2023-08-10 17:04:25 +0300

    Added Spring Data JPA dependency and required annotations

--- src/main/java/com/exercise/tasksmanagementsystem/entity/Task.java
+++ src/main/java/com/exercise/tasksmanagementsystem/entity/Task.java
@@ -1,6 +1,15 @@
 package com.exercise.tasksmanagementsystem.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private int timeSpent;
