ce2d7b0b5e0c8e9d323e746cc292e494824c9611
Liza <lixia101111@gmail.com>
2023-08-19 10:58:04 +0300

    Add TaskRepository

---
+++ src/main/java/com/exercise/tasksmanagementsystem/repository/TaskRepository.java
@@ -0,0 +1,8 @@
+package com.exercise.tasksmanagementsystem.repository;
+
+import com.exercise.tasksmanagementsystem.entity.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface TaskRepository extends JpaRepository<Task, Long> {
+}
