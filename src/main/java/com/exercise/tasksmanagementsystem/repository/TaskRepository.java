f7643545fab25566d9d914c13e426d7d3c28ca90
Liza <lixia101111@gmail.com>
2023-08-20 12:01:11 +0300

    Deleted empty line

--- src/main/java/com/exercise/tasksmanagementsystem/repository/TaskRepository.java
+++ src/main/java/com/exercise/tasksmanagementsystem/repository/TaskRepository.java
@@ -2,7 +2,5 @@ package com.exercise.tasksmanagementsystem.repository;
 
 import com.exercise.tasksmanagementsystem.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-
 public interface TaskRepository extends JpaRepository<Task, Long> {
 }
