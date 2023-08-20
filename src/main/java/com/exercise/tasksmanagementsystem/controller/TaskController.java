7754d2575266ee47cff2830e134bbb20446cb821
Liza <lixia101111@gmail.com>
2023-08-20 11:58:52 +0300

    Add saveTask

--- src/main/java/com/exercise/tasksmanagementsystem/controller/TaskController.java
+++ src/main/java/com/exercise/tasksmanagementsystem/controller/TaskController.java
@@ -1,5 +1,26 @@
 package com.exercise.tasksmanagementsystem.controller;
 
+import com.exercise.tasksmanagementsystem.entity.Task;
+import com.exercise.tasksmanagementsystem.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class TaskController {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @PostMapping("/save")
+    public ResponseEntity<Task> saveTask(@RequestBody Task task){
+        Task savedTask = taskRepository.save(task);
+        return new ResponseEntity<>(savedTask, HttpStatus.OK);
+    }
 
 }
