ee4b3830f1265a1e2ca81eca56b7317c18b5efc8
Liza <lixia101111@gmail.com>
2023-08-24 10:54:39 +0300

    Add findAll function

--- src/main/java/com/exercise/tasksmanagementsystem/controller/TaskController.java
+++ src/main/java/com/exercise/tasksmanagementsystem/controller/TaskController.java
@@ -5,10 +5,9 @@ import com.exercise.tasksmanagementsystem.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -21,6 +20,12 @@ public class TaskController {
     public ResponseEntity<Task> saveTask(@RequestBody Task task){
         Task savedTask = taskRepository.save(task);
         return new ResponseEntity<>(savedTask, HttpStatus.OK);
+    }
+
+    @GetMapping("/tasks")
+    public ResponseEntity<List<Task>> listAllTasks(){
+        List<Task> tasks = taskRepository.findAll();
+        return new ResponseEntity<>(tasks, HttpStatus.OK);
     }
 
 }
