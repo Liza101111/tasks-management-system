4fbae0c83a75bae41438b6c8a94eb78314491fb8
Liza <lixia101111@gmail.com>
2023-08-30 12:16:23 +0300

    Add findById function

--- src/main/java/com/exercise/tasksmanagementsystem/service/TaskService.java
+++ src/main/java/com/exercise/tasksmanagementsystem/service/TaskService.java
@@ -2,6 +2,7 @@ package com.exercise.tasksmanagementsystem.service;
 
 import com.exercise.tasksmanagementsystem.dto.TaskDto;
 import com.exercise.tasksmanagementsystem.entity.Task;
+import com.exercise.tasksmanagementsystem.exception.ResourceNotFoundException;
 import com.exercise.tasksmanagementsystem.mapper.TaskMapper;
 import com.exercise.tasksmanagementsystem.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,21 +10,17 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
-
 @Service
 public class TaskService {
     @Autowired
     private TaskRepository taskRepository;
-
     @Autowired
     private TaskMapper taskMapper;
-
     public TaskDto save(TaskDto taskDto){
         Task task = taskMapper.toEntity(taskDto);
-        task = taskRepository.save(task);
-        return taskMapper.toDto(task);
+        Task savedTask = taskRepository.save(task);
+        return taskMapper.toDto(savedTask);
     }
-
     public List<TaskDto> findAll(){
         List<Task> tasks = taskRepository.findAll();
         return taskMapper.toDtoList(tasks);
@@ -32,4 +29,11 @@ public class TaskService {
 //                .map(taskMapper::toDto)
 //                .collect(Collectors.toList());
     }
+
+    public TaskDto findById(Long taskId){
+        Task task = taskRepository.findById(taskId)
+                .orElseThrow(()-> new ResourceNotFoundException("Task not found with id: " + taskId));
+        return taskMapper.toDto(task);
+    }
+
 }
