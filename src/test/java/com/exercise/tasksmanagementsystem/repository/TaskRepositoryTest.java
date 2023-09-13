4faca440f9913d836517eb1e3a0acf3d01b79045
Liza <lixia101111@gmail.com>
2023-09-13 13:55:10 +0300

    Created TaskRepositoryTest

---
+++ src/test/java/com/exercise/tasksmanagementsystem/repository/TaskRepositoryTest.java
@@ -0,0 +1,12 @@
+package com.exercise.tasksmanagementsystem.repository;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+@DataJpaTest
+class TaskRepositoryTest {
+
+
+
+
+
+}
