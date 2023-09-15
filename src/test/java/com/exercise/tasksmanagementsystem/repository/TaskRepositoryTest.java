ff6cf442afde6265d1e56d2ec5ac89f64967f98d
Liza <lixia101111@gmail.com>
2023-09-15 11:28:49 +0300

    Add testFindAll

--- src/test/java/com/exercise/tasksmanagementsystem/repository/TaskRepositoryTest.java
+++ src/test/java/com/exercise/tasksmanagementsystem/repository/TaskRepositoryTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @DataJpaTest
 class TaskRepositoryTest {
@@ -31,6 +32,36 @@ class TaskRepositoryTest {
         //Then
         Assertions.assertThat(savedTask).isNotNull();
         Assertions.assertThat(savedTask.getId()).isGreaterThan(0);
+    }
+
+    @Test
+    public void testFindAll(){
+        Task task1 = new Task(
+                1L,
+                "Lucy",
+                LocalDateTime.now(),
+                LocalDateTime.now().plusDays(30),
+                TaskGroup.TASK_UNITY,
+                "Boss1"
+        );
+        Task task2 = new Task(
+                2L,
+                "Liza",
+                LocalDateTime.now(),
+                LocalDateTime.now().plusDays(100),
+                TaskGroup.TASK_FUSION,
+                "Boss2"
+        );
+
+        taskRepository.save(task1);
+        taskRepository.save(task2);
+
+        List<Task> taskList = taskRepository.findAll();
+
+        Assertions.assertThat(taskList.size()).isEqualTo(2);
+        for(Task task: taskList){
+            System.out.println(task);
+        }
     }
 
 }
