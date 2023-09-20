1c0c4e57d9b7ee3a77172f32b643964d684d93dd
Liza <lixia101111@gmail.com>
2023-09-20 10:13:43 +0300

    Create service test class

---
+++ src/test/java/com/exercise/tasksmanagementsystem/service/TaskServiceTest.java
@@ -0,0 +1,36 @@
+package com.exercise.tasksmanagementsystem.service;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TaskServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void updateTask() {
+    }
+
+    @Test
+    void deleteTask() {
+    }
+}
